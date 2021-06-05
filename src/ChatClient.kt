4c284d9362d807fcd666c0809880e3453f7e3e5a
jay_wu <jay_wu>
2021-06-05 11:39:05 +0800

    improve

---
+++ src/ChatClient.kt
@@ -0,0 +1,28 @@
+package com.example
+
+import io.ktor.client.features.websocket.*
+import io.ktor.http.cio.websocket.*
+
+suspend fun DefaultClientWebSocketSession.outputMessages() {
+    try {
+        for (message in incoming) {
+            message as? Frame.Text ?: continue
+            println(message.readText())
+        }
+    } catch (e: Exception) {
+        println("Error while receiving: " + e.localizedMessage)
+    }
+}
+
+suspend fun DefaultClientWebSocketSession.inputMessages() {
+    while (true) {
+        val message = readLine() ?: ""
+        if (message.equals("exit", true)) return
+        try {
+            send(message)
+        } catch (e: Exception) {
+            println("Error while sending: " + e.localizedMessage)
+            return
+        }
+    }
+}
