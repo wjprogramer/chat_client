422188a14fafe21bd7976a78a1a6f79008a898df
jay_wu <jay_wu>
2021-06-05 11:36:04 +0800

    create chat client

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,29 @@
 package com.example
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.client.*
+import io.ktor.client.features.websocket.*
+import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.util.*
+import kotlinx.coroutines.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+@KtorExperimentalAPI
+fun main() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+            while(true) {
+                val othersMessage = incoming.receive() as? Frame.Text ?: continue
+                println(othersMessage.readText())
+                val myMessage = readLine()
+                if(myMessage != null) {
+                    send(myMessage)
+                }
+            }
+        }
+    }
+    client.close()
+    println("Connection closed. Goodbye!")
 }
-
